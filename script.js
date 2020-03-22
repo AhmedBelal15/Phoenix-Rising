167be6d8b6ed13c40b7f64e91f966ce713cae6a4
AhmedBelal15 <62512711+AhmedBelal15@users.noreply.github.com>
2020-03-22 19:10:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const addbtn = document.getElementById("enter");
 const input = document.getElementById("userinput");
 const ul = document.getElementById("list");
 const lista = document.getElementsByTagName("li");
+const bodyS = document.querySelector("body");
 
 // create element on button click of Enter Press
 function createElement() {
@@ -28,13 +29,22 @@ input.addEventListener("keypress", addOnEnter);
 // End of Process 1
 
 // Toggle Done
-function doneTgl() {
-  for (let i = 0; i < lista.length; i++) {
-    lista[i].addEventListener("click", function() {
-      lista[i].classList.toggle("done");
-    });
-  }
-}
 
-doneTgl();
+// function doneTgl() {
+//   for (let i = 0; i < lista.length; i++) {
+//     lista[i].addEventListener("click", function() {
+//       lista[i].classList.toggle("done");
+//     });
+//   }
+// }
+
+// doneTgl();
 // end of Toggle Done
+
+// toggling done on click
+bodyS.addEventListener("click", function(event){
+  if (event.target.tagName.toLowerCase() === 'li'){
+      event.target.classList.toggle("done");
+  }
+})
+// end of process 2
